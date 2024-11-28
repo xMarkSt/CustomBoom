622f4570f3c309cfdabac6c8aa656fb9cf1f6a4a
Mark Stam <mstam@ambrac.nl>
2024-11-28 08:33:29 +0100

    Import scripts

---
+++ Scripts/targets.sql
@@ -0,0 +1,17 @@
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (1, 'Fastest time', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (2, 'All pickups', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (3, 'Complete Right Button', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (4, 'No coins', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (5, 'All bombs', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (6, 'Pickups', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (7, 'Touch ball', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (8, 'Bombs', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (9, 'Rockets', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (10, 'Use rocket', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (11, 'Only one rocket', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (12, 'Finish with rocket', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (13, 'Springboards', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (14, 'All jumpboards', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (15, 'Touch star', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (16, 'Break ball', null, null);
+INSERT INTO boomclone.targets (id, type, created_at, updated_at) VALUES (17, 'Touch bowling pin', null, null);
---
+++ Scripts/themes.sql
@@ -0,0 +1,7 @@
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (1, 'Jurassic Jungle', null, null, 'JungleBG.plist');
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (2, 'Lost City', null, null, 'CityBG.plist');
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (3, 'Frozen Hills', null, null, 'NorthBG.plist');
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (4, 'Pyramid Dunes', null, null, 'EgyptBG.plist');
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (5, 'Robot Factory', null, null, 'FactoryBG.plist');
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (6, 'Festival Park', null, null, 'FestivalParkBG.plist');
+INSERT INTO boomclone.themes (id, name, created_at, updated_at, bg_name) VALUES (7, 'Extreme', null, null, null);
